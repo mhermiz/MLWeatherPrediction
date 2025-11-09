c2ae03bb1f7243176d346a98c4024f3a8c7de353
Mario Hermiz <134554555+mhermiz@users.noreply.github.com>
2025-11-08 17:22:01 -0800

    Added dataset and KNNModel to classify weather variable
    - Added seattle-weather.csv dataset
    - Initialized a KNN model to classify target variable

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+# Load dataset
+weatherdata = pd.read_csv('seattle-weather.csv')
+
+# Display first few rows of the dataset
+print(weatherdata.head())
+
+# Define features and target variable
+X = weatherdata[['temp_min', 'precipitation', 'wind']]
+y = weatherdata['weather']
+
+# Intialize model with 5 neighbors
+knnModel = KNeighborsClassifier(n_neighbors=3)
+
+# fit the model
+knnModel.fit(X, np.ravel(y))
+
+# Calculate prediction for each instance in X
+predictions = knnModel.predict(X)
+print("Predictions:", predictions)
+
+# Calculate accuracy
+accuracy = knnModel.score(X, np.ravel(y))
+print("Accuracy:", accuracy)
